f4548dab3233618f45ea4e2bfa046381fbd53f8d
hyojin park <candy020406@naver.com>
2022-11-23 21:47:52 +0900

    ㄴ

---
+++ chapter9/6.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace chapter9
+{
+    partial class _6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // _6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 214);
+            this.Name = "_6";
+            this.Text = "_6";
+            this.Load += new System.EventHandler(this._6_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
