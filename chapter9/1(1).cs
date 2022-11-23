a089e674edbe75d4586df458506705c2e8788b9e
hyojin park <candy020406@naver.com>
2022-11-23 21:36:10 +0900

    ㄴ

--- chapter9/1(1).cs
+++ chapter9/1(1).cs
@@ -19,7 +19,7 @@ namespace chapter9
 
         private void button1_Click(object sender, EventArgs e) //닫기 버튼 이벤트 처리기
         {
-            this.Close();
+            this.Close(); //Form1창 닫기
         }
     }
 }
