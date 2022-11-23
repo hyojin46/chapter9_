baea4d028e2198b3183abfc5c4fee42937f12cbe
hyojin park <candy020406@naver.com>
2022-11-23 21:41:22 +0900

    ㄴ

---
+++ chapter9/3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chapter9
+{
+    public partial class _3 : Form
+    {
+        public _3()
+        {
+            InitializeComponent();
+        }
+
+        private void _3_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("MessageBox", "Title Bar")
+        }
+    }
+}
