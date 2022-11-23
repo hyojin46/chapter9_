a32a84ca36b4dfe0ed41d13398befdb70affeb3d
hyojin park <candy020406@naver.com>
2022-11-23 21:49:48 +0900

    ㄴ

--- chapter9/7.cs
+++ chapter9/7.cs
@@ -16,5 +16,15 @@ namespace chapter9
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = @"C:\";
+            openFileDialog1.Filter = "텍스트 파일(*.txt)|*.txt|모든 파일(*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.ShowDialog();
+            textBox1.Text = openFileDialog1.FileName;
+        }
     }
 }
