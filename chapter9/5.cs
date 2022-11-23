e4d97a5752c714713ea8995743f00953fec45002
hyojin park <candy020406@naver.com>
2022-11-23 21:45:30 +0900

    ㄴ

---
+++ chapter9/5.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chapter9
+{
+    public partial class _5 : Form
+    {
+        public _5()
+        {
+            InitializeComponent();
+        }
+        private int i; // 필드 :멤버 변수
+        private void _5_Load(object sender, EventArgs e)
+        {
+            i = i < 64 ? i += 16 : 0;
+            this.Text = ((MessageBoxIcon)i).ToString();
+            MessageBox.Show("MessageBoxIcon", "Title Bar",MessageBoxButtons.OKCancel, (MessageBoxIcon)i);
+        }
+    }
+}
