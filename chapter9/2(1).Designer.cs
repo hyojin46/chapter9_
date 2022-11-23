d5308fd390686f8e00ca06a624de46c89fbf0955
hyojin park <candy020406@naver.com>
2022-11-23 21:40:05 +0900

    ㄴ

--- chapter9/2(1).Designer.cs
+++ chapter9/2(1).Designer.cs
@@ -38,7 +38,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(151, 61);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "닫기";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // label1
