9b0f447dee3f3d4c2ba8b6b18326e5e9396af7ed
hyojin park <candy020406@naver.com>
2022-11-23 21:39:58 +0900

    ㄴ

--- chapter9/2(1).Designer.cs
+++ chapter9/2(1).Designer.cs
@@ -46,9 +46,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(19, 36);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.Size = new System.Drawing.Size(105, 12);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "모달리스 대화상자";
             // 
             // _2_1_
             // 
