490d6c2b8cee53257502c25120ccdfce9784fa42
hyojin park <candy020406@naver.com>
2022-11-23 21:39:48 +0900

    ㄴ

--- chapter9/2(1).Designer.cs
+++ chapter9/2(1).Designer.cs
@@ -28,19 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
             // 
             // _2_1_
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(184, 161);
+            this.ClientSize = new System.Drawing.Size(194, 161);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Name = "_2_1_";
             this.Text = "_2_1_";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
