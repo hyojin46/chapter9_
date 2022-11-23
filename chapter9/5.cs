f78b0c43e564e879552ee5d56f630075d299df13
hyojin park <candy020406@naver.com>
2022-11-23 21:45:49 +0900

    ㄴ

--- chapter9/5.cs
+++ chapter9/5.cs
@@ -17,7 +17,7 @@ namespace chapter9
             InitializeComponent(); //초기화
         }
         private int i; // 필드 :멤버 변수
-        private void _5_Load(object sender, EventArgs e)
+        private void _5_Load(object sender, EventArgs e) //폼 로드한 경우
         {
             i = i < 64 ? i += 16 : 0;
             this.Text = ((MessageBoxIcon)i).ToString();
